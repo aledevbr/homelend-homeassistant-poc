25eb514368635da1f9f08d27a4d9c62aa3057b15
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2024-01-17 19:49:35 -0300

    retirado o hsts do dotnet

--- HttpInfo/server.js
+++ HttpInfo/server.js
@@ -1,11 +1,5 @@
 'use strict';
-//var http = require('http');
 var port = process.env.PORT || 8080;
-
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
 
 const express = require('express');
 const cors = require('cors');
