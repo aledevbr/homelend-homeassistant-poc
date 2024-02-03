bc06b8dcc94d231f58b02b834ab6e27c9263d6b7
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2024-02-02 22:42:51 -0300

    Criando as pastas via start.sh

--- RabbitMQ/start.sh
+++ RabbitMQ/start.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
 echo "Listando arquivos da pasta /data"
-
 ls -lah /data
+
+echo "criando pastas para o RabbitMQ"
+mkdir -p /data/rabbitmq/mnesia
+mkdir -p /data/rabbitmq/logs
+
+echo "Alterando permissões das pastas"
+chown -R rabbitmq:rabbitmq /data/rabbitmq
+
+echo "Listando arquivos da pasta /data (2)"
+ls -lah /data
+
 
 # Iniciando o servidor RabbitMQ
 echo "Iniciando o servidor RabbitMQ"
